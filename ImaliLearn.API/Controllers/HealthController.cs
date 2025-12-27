bbb05408aee6ec272c033b0c5d82c4c86491ecc6
Jaydin Morrison <4260354@myuwc.ac.za>
2025-12-28 00:14:35 +0200

    feat: add clean archicecture solution structure

---
+++ ImaliLearn.API/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+/*
+ Why this matters
+
+Verifies pipeline, hosting, and routing
+
+Used by load balancers in production
+ */
+namespace ImaliLearn.API.Controllers;
+[ApiController]
+[Route("api/health")]
+public class  HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHealthStatus()
+    {
+        return Ok(new { status = "Healthy", timestamp = DateTime.UtcNow });
+    }
+}
+
